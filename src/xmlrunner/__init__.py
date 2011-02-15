ad766d1e64be8bdb6014524ade3e928bb93f0cb7
Nicholas Avenell <nicholas@languagelab.com>
2011-02-15 13:33:22 +0000

    Ignore tests functionality

--- src/xmlrunner/__init__.py
+++ src/xmlrunner/__init__.py
@@ -285,7 +285,8 @@ class XMLTestRunner(TextTestRunner):
     def run(self, test):
         "Run the given test case or test suite."
 
-        test = reorder_suite(test, (TestCase,))
+        #test = reorder_suite(test, (TestCase,))
+        print "{XX} Testing %s" % test
 
         try:
             # Prepare the test execution
@@ -346,50 +347,52 @@ class XMLTestRunner(TextTestRunner):
 #                    tests.append(case)
 #            suite._tests = tests
 #        return suite
-
-
-# From django.test.simple.DjangoTestRunner
-def reorder_suite(suite, classes):
-    """
-    Reorders a test suite by test type.
-
-    classes is a sequence of types
-
-    All tests of type clases[0] are placed first, then tests of type classes[1], etc.
-    Tests with no match in classes are placed last.
-    """
-    class_count = len(classes)
-    bins = [TestSuite() for i in range(class_count+1)]
-    partition_suite(suite, classes, bins)
-    for i in range(class_count):
-        bins[0].addTests(bins[i+1])
-    return bins[0]
-
-def partition_suite(suite, classes, bins):
-    """
-    Partitions a test suite by test type.
-
-    classes is a sequence of types
-    bins is a sequence of TestSuites, one more than classes
-
-    Tests of type classes[i] are added to bins[i],
-    tests with no match found in classes are place in bins[-1]
-    """
-    for test in suite:
-        if isinstance(test, TestSuite):
-            partition_suite(test, classes, bins)
-        else:
-            pkg = test.__class__.__module__.split('.')[0]
-            print "This is %s" % pkg
-            if pkg in EXCLUDED_APPS:
-                print "... ignoring it"
-                continue
-
-            print "... testing it"
-
-            for i in range(len(classes)):
-                if isinstance(test, classes[i]):
-                    bins[i].addTest(test)
-                    break
-            else:
-                bins[-1].addTest(test)
+#
+#
+## From django.test.simple.DjangoTestRunner
+#def reorder_suite(suite, classes):
+#    """
+#    Reorders a test suite by test type.
+#
+#    classes is a sequence of types
+#
+#    All tests of type clases[0] are placed first, then tests of type classes[1], etc.
+#    Tests with no match in classes are placed last.
+#    """
+#    class_count = len(classes)
+#    bins = [TestSuite() for i in range(class_count+1)]
+#    partition_suite(suite, classes, bins)
+#    for i in range(class_count):
+#        bins[0].addTests(bins[i+1])
+#    return bins[0]
+#
+#def partition_suite(suite, classes, bins):
+#    """
+#    Partitions a test suite by test type.
+#
+#    classes is a sequence of types
+#    bins is a sequence of TestSuites, one more than classes
+#
+#    Tests of type classes[i] are added to bins[i],
+#    tests with no match found in classes are place in bins[-1]
+#    """
+#    for test in suite:
+#        if isinstance(test, TestSuite):
+#            partition_suite(test, classes, bins)
+#        else:
+#            pkg = test.__class__.__module__.split('.')[0]
+#            subpkg = ".".join(test.__class__.__module__.split('.')[0:2] )
+#            if subpkg in EXCLUDED_APPS:
+#                print "ignoring S %s" % subpkg
+#                continue
+#            elif pkg in EXCLUDED_APPS:
+#                print "ignoring P %s" % pkg
+#                continue
+#
+#            print "testing %s %s " % (pkg, subpkg)
+#            for i in range(len(classes)):
+#                if isinstance(test, classes[i]):
+#                    bins[i].addTest(test)
+#                    break
+#            else:
+#                bins[-1].addTest(test)
--- src/xmlrunner/extra/djangotestrunner.py
+++ src/xmlrunner/extra/djangotestrunner.py
@@ -10,6 +10,11 @@ Django docs website.
 
 from django.test.simple import *
 import xmlrunner
+import unittest
+import copy
+
+from django.conf import settings
+EXCLUDED_APPS = getattr(settings, 'TEST_EXCLUDE', [])
 
 class XMLTestRunner(DjangoTestSuiteRunner):
     def run_tests(self, test_labels, verbosity=1, interactive=True, extra_tests=[]):
@@ -25,22 +30,55 @@ class XMLTestRunner(DjangoTestSuiteRunner):
 
         When looking for tests, the test runner will look in the models and
         tests modules for the application.
-        
+
         A list of 'extra' tests may also be provided; these tests
         will be added to the test suite.
-        
+
         Returns the number of tests that failed.
         """
-        setup_test_environment()
-        
+
         settings.DEBUG = False
-        
         verbose = getattr(settings, 'TEST_OUTPUT_VERBOSE', False)
         descriptions = getattr(settings, 'TEST_OUTPUT_DESCRIPTIONS', False)
         output = getattr(settings, 'TEST_OUTPUT_DIR', '.')
+
+        self.setup_test_environment()
+        suite = self.build_suite(test_labels, extra_tests)
+
+        #suite = self.filter_excluded_tests(suite);
         
+        old_config = self.setup_databases()
+
+        result = xmlrunner.XMLTestRunner(
+            verbose=verbose, descriptions=descriptions, output=output).run(suite)
+
+        self.teardown_databases(old_config)
+        self.teardown_test_environment()
+        return self.suite_result(suite, result)
+
+#    def build_suite(self, test_labels, extra_tests=None, **kwargs):
+#        suite = unittest.TestSuite()
+#
+#        if test_labels:
+#            for label in test_labels:
+#                if '.' in label:
+#                    suite.addTest(build_test(label))
+#                else:
+#                    app = get_app(label)
+#                    suite.addTest(build_suite(app))
+#        else:
+#            for app in get_apps():
+#                suite.addTest(build_suite(app))
+#
+#        if extra_tests:
+#            for test in extra_tests:
+#                suite.addTest(test)
+#
+#        return reorder_suite(suite, (TestCase,))
+
+    def build_suite(self, test_labels, extra_tests=None, **kwargs):
         suite = unittest.TestSuite()
-        
+
         if test_labels:
             for label in test_labels:
                 if '.' in label:
@@ -50,17 +88,53 @@ class XMLTestRunner(DjangoTestSuiteRunner):
                     suite.addTest(build_suite(app))
         else:
             for app in get_apps():
+                pkg = app.__name__
+
+                if self.is_excluded(pkg):
+                    print "Deleting %s" % pkg
+                    #del suite._tests[key]
+                else:
+                    print "Keeping  %s" % pkg
                 suite.addTest(build_suite(app))
-        
-        for test in extra_tests:
-            suite.addTest(test)
 
-        old_config = self.setup_databases()
+        if extra_tests:
+            for test in extra_tests:
+                pass
+                #suite.addTest(test)
 
-        result = xmlrunner.XMLTestRunner(
-            verbose=verbose, descriptions=descriptions, output=output).run(suite)
-        
-        self.teardown_databases(old_config)
-        teardown_test_environment()
-        
-        return len(result.failures) + len(result.errors)
+        return reorder_suite(suite, (TestCase,))
+
+    def filter_excluded_tests(self, suite):
+        tests = unittest.TestSuite()
+
+
+        keys = range(0, len(suite._tests))
+        keys.reverse()
+
+        for key in keys:
+            test = suite._tests[key]
+            if isinstance(test, unittest.TestSuite):
+                print "Entering suite... "
+                tests.addTests(self.filter_excluded_tests(test))
+            else:
+                if self.is_excluded(test):
+                    print "Deleting %s" % test
+                    #del suite._tests[key]
+                else:
+                    print "Keeping  %s" % test
+
+        return suite
+
+    def is_excluded(self, name):
+        pkg = name.split('.')[0]
+        subpkg = ".".join(name.split('.')[0:2] )
+        if subpkg in EXCLUDED_APPS:
+            #print "ignoring S %s" % subpkg
+            return True
+        elif pkg in EXCLUDED_APPS:
+            #print "ignoring P %s" % pkg
+            return True
+
+        print "Testing %s " % subpkg
+
+        return False
